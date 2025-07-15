afaa7c739eae4b14f0344fe39deb6706c0fd7137
priyanka <priyankakommani@gmail.com>
2025-07-15 18:24:55 +0530

    Update index file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { Button } from '~/components/ui/button'
 export const Route = createFileRoute('/')({
   component: Home,
 })
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <div className="p-2">
       <h3>Welcome Home!!!</h3>
-      <Button className='mt-2 bg-green-600'>Click Me</Button>
+      <Button className='mt-2 bg-green-600' onClick={() => navigate({ to: '/users' })} >Click Me</Button>
     </div>
   )
 }
